f15a00f502387528825570974fbc0140f2f9b740
Hintee <hintea_dan@yahoo.co.uk>
2017-08-22 01:21:53 +0300

    Scaffold Solution

--- Code/Code Samples/CSharpCodeSamples/CSharpCodeSamples/Program.cs
+++ Code/Code Samples/CSharpCodeSamples/CSharpCodeSamples/Program.cs
@@ -10,6 +10,15 @@ namespace CSharpCodeSamples
     {
         static void Main(string[] args)
         {
+            byte byteMe = 2;
+            int numberOfStars = 42;
+            float floatingOnAir = 3.1415f;
+            double decimalAir = 3.141542;
+            string myName = "Can't remember";
+
+            var random = new Random();
+
+
             Console.WriteLine("Hello from C#");
             Console.ReadLine();
         }
